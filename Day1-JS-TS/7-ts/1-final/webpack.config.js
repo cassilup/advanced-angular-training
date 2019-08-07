377e6673f522ca05b3f0327f2462c2ba75bfc401
Cassi Lup <cassi.lup@gmail.com>
2019-08-07 09:03:22 +0300

    TS

---
+++ Day1-JS-TS/7-ts/1-final/webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [{
+      test: /\.js/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env']
+        }
+      }
+    }]
+  },
+  plugins: [
+    new HtmlWebpackPlugin()
+    new TsconfigPathsPlugin({ configFile: "./tsconfig.json" })
+  ],
+  devServer: {  contentBase: path.join(__dirname, "dist") },
+};
+
+
+
+
+
+
+
+// var HtmlWebpackPlugin = require('html-webpack-plugin');
+// plugins: [new HtmlWebpackPlugin()]
+
+// devServer: {
+//   contentBase: path.join(__dirname, "dist")
+// },
