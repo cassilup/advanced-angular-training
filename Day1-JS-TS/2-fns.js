6b846a7d6b41b5895b5ebc04072542d9b346f7f0
Cassi Lup <cassi.lup@gmail.com>
2019-08-07 08:45:21 +0300

    Day1

---
+++ Day1-JS-TS/2-fns.js
@@ -0,0 +1,17 @@
+// Simple Fn
+function sayHiSimpleFn() {
+  console.log('hi');
+}
+
+// Const Fn
+const sayHiConstFn = function() {
+  console.log('hi (const)');
+}
+
+// Function as a variable.
+
+const sayHiArrowFn = () => {
+  console.log('hi (arrow)');
+}
+
+const sayHiCondensedArrowFn = () => console.log('hi (condensed)');
---
+++ Day1-JS-TS/3-arrow-fns.js
@@ -0,0 +1,18 @@
+// Arrow Functions
+
+// Before
+function sayHi() {
+  console.log('hi');
+}
+
+// After
+const sayHi2 = () => {
+  console.log('hi');
+};
+
+// Even shorter
+const sayHi3 = () => console.log('hi');
+
+sayHi();
+sayHi2();
+sayHi3();
